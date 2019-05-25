403ac422104955448284f362e779ff0897661e98
robotwxy <WANG1177@e.ntu.edu.sg>
2019-05-25 15:15:46 +0800

    rm wechat id/secrect

--- go_src/myweixin/main.go
+++ go_src/myweixin/main.go
@@ -12,19 +12,12 @@ var mp *weixinmp.Weixinmp
 var counter int = 1
 
 func main() {
-	/*
-		token := "wxy1994812"                        // 微信公众平台的Token
-		appid := "wx2691dccf0c9cd63a"                // 微信公众平台的AppID
-		secret := "d5c062193ac8e546d0938b05d5bf8bdd" // 微信公众平台的AppSecret
-	*/
-	token := "wxytest"                           // 微信公众平台的Token test
-	appid := "wx15e22c8145dc256f"                // 微信公众平台的AppID
-	secret := "a2eaf380a52039236782f14b08acc1a2" // 微信公众平台的AppSecret
-	// 仅被动响应消息时可不填写appid、secret
-	// 仅主动发送消息时可不填写token
+
+	token := "xxxxxxxxxx"            // wechat Token test
+	appid := "xxxxxxxxxxxxxxxxxx"    // wechat AppID
+	secret := "xxxxxxxxxxxxxxxxxxxx" // wechat AppSecret
 	mp = weixinmp.New(token, appid, secret)
 	mp.AccessToken.Fresh()
-	// 注册处理函数
 	http.HandleFunc("/receiver", receiver)
 	http.HandleFunc("/forwarder", forwarder)
 	log.Fatal(http.ListenAndServe(":80", nil))
@@ -45,22 +38,18 @@ func forwarder(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(string(raw))
 	msgRemain = msgRemain - 1
 	str := fmt.Sprintf("sms: \n%s \n ************* \n%d messages can be sent\n", string(raw), msgRemain)
-	str1, err := mp.SendTextMsg("oFJmg53dfpTrGse53V0PifOE7xY0", str)
+	str1, err := mp.SendTextMsg("xxxxxxxxxxxxxxxxxx", str) // user id
 	w.Write([]byte(str1))
 	counter = counter + 1
 }
 func receiver(w http.ResponseWriter, r *http.Request) {
-	// 检查请求是否有效
-	// 仅主动发送消息时不用检查
 	if !mp.Request.IsValid(w, r) {
 		return
 	}
 	msgRemain = 20
 	fmt.Println(mp.Request.MsgType, "from", mp.Request.FromUserName)
 	fmt.Println(mp.Request.MsgType, "from", mp.Request.ToUserName)
-	// 判断消息类型
 	if mp.Request.MsgType == weixinmp.MsgTypeText {
-		// 回复消息
-		mp.ReplyTextMsg(w, "Hello, 世界")
+		mp.ReplyTextMsg(w, "Hello, world")
 	}
 }
